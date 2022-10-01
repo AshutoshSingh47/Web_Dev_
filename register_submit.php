049a960888dce16970a253f991162837a6d4ffad
muske <ashutoshsingh.iimt@gmail.com>
2022-10-01 19:16:41 +0530

    new project

---
+++ register_submit.php
@@ -0,0 +1,18 @@
+<?php
+
+    $fname = $_POST['fname'];
+    $lname = $_POST['lname'];
+    $gender = $_POST['gender'];
+    $phone_no = $_POST['phone_no'];
+    $email = $_POST['email'];
+    $address = $_POST['address'];
+    $date_of_birth = $_POST['date_of_birth'];
+
+    echo $fname . "<br/>";
+    echo $lname . "<br/>";
+    echo $gender . "<br/>";
+    echo $phone_no . "<br/>";
+    echo $email . "<br/>";
+    echo $address . "<br/>";
+    echo $date-of_birth . "<br/>";
+?>
